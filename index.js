51c4e6d5ed73b828a08a8d3f27ff28e6553b19ce
Chetan <chetan.mogha@gmail.com>
2021-09-25 03:01:41 +0530

    Add Contact feature

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const port = 8000;
 
 const app = express();
-const kil;
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -30,6 +30,10 @@ app.get('/', function(req, res){
     });
 })
 
+app.post("/create-contact",(req,res)=>{
+
+    console.log(req.body)
+})
 
 app.listen(port, function(err){
     if (err) {
