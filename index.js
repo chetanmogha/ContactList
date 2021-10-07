ecd26c3ea2dd39f407902e3279ddd5f40c4bdbab
Chetan <chetan.mogha@gmail.com>
2021-10-08 02:26:35 +0530

    Remove array of contact list

--- index.js
+++ index.js
@@ -11,20 +11,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded());
 app.use(express.static('assets'));
 
-var contactList = [
-    {
-        name: "Arpan",
-        phone: "1111111111"
-    },
-    {
-        name: "Tony Stark",
-        phone: "1234567890"
-    },
-    {
-        name: "Coding Ninjas",
-        phone: "12131321321"
-    }
-]
+
+    
 
 app.get('/', function(req, res){
 
@@ -44,10 +32,7 @@ app.get('/', function(req, res){
 })
 
 app.post("/create-contact",(req,res)=>{
-    // contactList.push({
-    //     name:req.body.name,
-    //     phone:req.body.phone
-    // })
+    
 
     Contact.create({
         name:req.body.name,
@@ -77,16 +62,12 @@ app.get("/delete-contact/",(req,res)=>{
         res.redirect('back');
 
     })
-    // const contactIndex=contactList.findIndex(contact => contact.phone==phone);
-    // if(contactIndex!=-1){
-    //     contactList.splice(contactIndex,1);
-    // }
-   
+  
 })
 
 app.listen(port, function(err){
     if (err) {
         console.log("Error in running the server", err);
     }
-    console.log('Yup!My Server is running on Port', port);
+    console.log('Server is running at Port', port);
 })
